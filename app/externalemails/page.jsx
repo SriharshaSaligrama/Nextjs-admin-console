c01e453680fcc72f4fe17ccb716cc5a10bc49f17
Sri Harsha <sriharshasaligrama@gmail.com>
2024-03-23 21:02:57 +0530

    made external emails route dynamic

--- app/externalemails/page.jsx
+++ app/externalemails/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ExternalEmailsHome from '../_lib/components/features/externalemails'
 import { getAllExternalEmails } from '../_lib/db/groups/controller'
+import { unstable_noStore as noStore } from 'next/cache';
 
 export const metadata = {
     title: 'External Emails',
 }
 
 const ExternalEmails = async () => {
+    noStore()
     const data = await getAllExternalEmails()
 
     return (
